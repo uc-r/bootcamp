a689c2045acf0a467b25f57f99285a5487117c0f
Brad Boehmke <bradleyboehmke@gmail.com>
2016-07-29 14:48:31 -0400

    imported all class files for the students

---
+++ code scripts/3-data-structures-students.R
@@ -0,0 +1,174 @@
+# Session 3: Understanding Data Structures
+
+
+###########
+# Vectors #
+###########
+# Notes: 
+
+# Creating vectors
+
+
+# Understanding vector properties
+facebook <- read.delim("data/facebook.tsv")
+likes <- facebook$likes
+
+# Extracting/subsetting vectors (vector[i])
+
+
+# Comparison operators
+
+
+# Subsetting data based on comparison operators
+
+
+# Summarizing vectors
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Save the friend_count variable from the facebook data as a vector:
+
+fc <- facebook$friend_count
+
+# 1. What are the min, max, median, and mean number of friends?
+
+# 2. How many people have 0 friends?
+
+# 3. How many people have 1,000 or more friends?
+
+# 4. Which element contains the maximum number of friends?
+
+
+
+
+
+############
+# Matrices #
+############
+# Notes: 
+
+# Creating matrices
+
+
+# Understanding matrix properties
+m <- matrix(runif(12), nrow = 3)
+
+
+# Extracting/subsetting matrices (matrix[row, col])
+
+
+# Comparison operators
+
+
+# Summarizing matrices
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Save the friend_count variable from the facebook data as a vector:
+
+fc <- facebook$friend_count
+
+# 1. Subset fc to find individuals with more than 4,600 friends
+
+
+# 2. Turn this subsetted vector into a matrix with 10 rows
+
+
+# 3. What are the dimensions of this matrix
+
+
+
+
+
+
+############
+# Lists #
+############
+# Notes: 
+
+# Creating lists
+l1 <- list(item1 = 1:3,
+           item2 = letters[1:5],
+           item3 = c(T, F, T, T),
+           item4 = matrix(1:9, nrow = 3))
+
+
+# Understanding list properties
+
+
+# Extracting/subsetting lists 
+
+
+# What you need to know - how to get results out of statistical models
+model <- lm(mpg ~ wt, data = mtcars)    # linear regression model
+mode(model)
+str(model)
+names(model)
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Create this linear regression model:
+
+fb_model <- lm(friend_count ~ gender + age, data = facebook)
+
+# 1. Extract the residuals from the fb_model list
+
+
+# 2. What is the min, max, median, and mean of these residuals?
+
+
+
+
+
+
+###############
+# Data Frames #
+###############
+# Notes: 
+
+# Creating data frames
+df <- data.frame(variable.1 = 1:3,
+                 variable.2 = c("a", "b", "c"),
+                 variable.3 = c(TRUE, TRUE, FALSE))
+
+
+# Understanding data frame properties
+
+
+# Extracting/subsetting data frames (data.frame[row, col])
+
+
+# Conditional subsetting data frames (subset[data.frame, row condition, column selection])
+
+
+# Summarizing data frames
+
+
+
+##############
+# YOUR TURN! #
+##############
+# 1. Import the reddit data located at https://bradleyboehmke.github.io/public/data/reddit.csv
+
+
+# 2. What variables (column names) does this data frame contain?
+
+
+# 3. How many users have the employment status of "Student"?
+
+
+# 4. Subset the reddit data frame for only those individuals that are students.
+
+
+
+
+
---
+++ code scripts/6-dplyr-students.R
@@ -0,0 +1,255 @@
+# Session 6: Transforming & manipulating data sets with dplyr
+
+
+################
+# Requirements #
+################
+# install.packages("dplyr")
+# devtools::install_github("rstudio/EDAWR")
+
+library(dplyr)
+library(EDAWR)
+
+# data used (from the EDAWR package)
+storms
+tb
+pollution
+iris
+a
+b
+
+
+############
+# select() #
+############
+# Notes: 
+
+
+# these two options are the same
+select(storms, storm, pressure)
+storms %>% select(storm, pressure)
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Import the facebook.tsv file in the data folder
+
+
+# Create a new data frame that includes: userid, age, gender, friend_count
+
+
+
+
+
+############
+# filter() #
+############
+# Notes: 
+
+
+# these two options are the same
+filter(storms, wind >= 50)
+storms %>% filter(wind >= 50)
+
+# can filter for multiple conditions
+storms %>% filter(wind >= 50,
+                  storm %in% c("Alberto", "Alex", "Allison"))
+
+
+
+##############
+# YOUR TURN! #
+##############
+# 1. Filter the facebook data for just males
+
+
+# 2. Select the userid, age, gender, friend_count variables and then 
+#    filter for males
+
+
+
+
+##############
+# group_by() #
+##############
+# Notes: 
+
+
+# these two options are the same
+group_by(tb, country)
+tb %>% group_by(country)
+
+# group by multiple categorical variables
+tb %>% group_by(country, year)
+
+
+
+##############
+# YOUR TURN! #
+##############
+# 1. Select the userid, age, gender, friend_count variables variables and 
+# then group by gender
+
+
+
+
+
+###############
+# summarise() #
+###############
+# Notes: 
+
+
+iris
+
+# these are the same
+summarise(iris, Avg_SL = mean(Sepal.Length))
+iris %>% summarise(Avg_SL = mean(Sepal.Length))
+
+# get multiple summary statistics of a single variable
+iris %>% summarise(mean_SL = mean(Sepal.Length),
+                   med_SL = median(Sepal.Length),
+                   SD_SL = sd(Sepal.Length),
+                   n = n())
+
+
+# combine group_by() and summarise()
+iris %>% 
+        group_by(Species) %>%
+        summarise(mean_SL = mean(Sepal.Length),
+                  med_SL = median(Sepal.Length),
+                  SD_SL = sd(Sepal.Length),
+                  n = n())
+
+# get summary statistics of multiple variables
+iris %>% 
+        group_by(Species) %>%
+        summarise_each(funs(mean))
+
+iris %>% 
+        group_by(Species) %>%
+        summarise_each(funs(mean, sd))
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Continuing with our facebook data...
+# 1. Calculate the mean and median for tenure and friend_count
+
+
+# 2. Select the gender, age, and friend_count variables, group by gender, 
+# and calculate mean and median
+
+
+
+
+#############
+# arrange() #
+#############
+# Notes: 
+
+
+iris %>% arrange(Sepal.Length)
+iris %>% arrange(desc(Sepal.Length))
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Continuing with our facebook data...
+# 1. Group the data by age
+# 2. Calculate the median friend_count for each age
+# 3. Arrange the median calculated friend_count variable in descending order 
+# to find the ages with the greatest median friend count
+
+
+
+
+############
+# mutate() #
+############
+# Notes: 
+
+
+# add a new variable to the iris data
+iris
+iris %>% mutate(Sepal.Area = Sepal.Length * Sepal.Width)
+
+# create a new variable but drop all the original columns
+iris %>% transmute(Sepal.Area = Sepal.Length * Sepal.Width)
+
+# apply function to each column - in this case get the ranking of each
+# value for each variable
+iris %>% mutate_each(funs(min_rank))
+
+
+
+##############
+# YOUR TURN! #
+##############
+# Continuing with our facebook data...
+# 1. Create a new variable `friend_ratio` that equals friendships_initiated / friend_count
+# 2. Find the median `friend_ratio` for males and females
+
+
+
+
+#########
+# joins #
+#########
+# Notes: 
+
+
+# there are different ways to join a and b
+a
+b
+
+# left_join
+left_join(a, b, by = "x1")
+a %>% left_join(b)
+
+# right_join
+right_join(a, b, by = "x1")
+a %>% right_join(b, by = "x1")
+
+# inner_join
+inner_join(a, b, by = "x1")
+a %>% inner_join(b, by = "x1")
+
+# full_join
+full_join(a, b, by = "x1")
+a %>% full_join(b, by = "x1")
+
+# semi_join
+semi_join(a, b, by = "x1")
+a %>% semi_join(b, by = "x1")
+
+# anti_join
+anti_join(a, b, by = "x1")
+a %>% anti_join(b, by = "x1")
+
+
+
+##############
+# YOUR TURN! #
+##############
+# 1. Import the reddit.csv and regions.csv files in the data folder
+
+
+
+# 2. Join the regions data to the reddit data
+
+
+
+
+
+
+
+
+
+
